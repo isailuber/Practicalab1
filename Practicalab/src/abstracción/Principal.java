d5cdba44942fbe33b8cb8d6cfaf641793d5e6dba
HOME <HOME@HOME-PC>
2021-02-27 01:55:11 -0500

    se instanciaron los objetos y se agregaron a la función ejecutable

--- Practicalab/src/abstracción/Principal.java
+++ Practicalab/src/abstracción/Principal.java
@@ -3,7 +3,19 @@ package abstracción;
 public class Principal {
 
 	public static void main(String[] args) {
-		
+
+		// Esta variable solo puede invocar metodos de la clase SumaRestaEntreNumeros.
+		// No puede invocar metodos de la clase OperacionesEntreNumeros.
+		SumaRestaEntreNumeros variable = new OperacionesEntreNumeros();
+
+		// esta variable si puede invocar metodos de la clase OperacionesEntreNumeros
+		// que sean publicos. Caso contrario, no puede.
+		OperacionesEntreNumeros variable2 = new OperacionesEntreNumeros();
+
+		System.out.println("invocando restar: " + variable.restar(5, 2));
+
+		System.out.println("invocado sumar: " + variable2.sumar(5, 2));
+
 	}
 
 }
