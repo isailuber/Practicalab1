c405e9238eba65498bbc1c9a1feacaa6c55744d9
HOME <HOME@HOME-PC>
2021-02-27 01:41:43 -0500

    a la clase abstracta resta se le crea su función

--- Practicalab/src/abstracción/OperacionesEntreNumeros.java
+++ Practicalab/src/abstracción/OperacionesEntreNumeros.java
@@ -1,5 +1,17 @@
 package abstracción;
 
-public class OperacionesEntreNumeros {
+public class OperacionesEntreNumeros extends SumaRestaEntreNumeros {
+	@Override
+	public int restar(int argumento1, int argumento2) {
+		return argumento1 - argumento2;
+	}
+
+	// llama al metodo protected de la clase publica.
+	public int sumar(int argumento1, int argumento2) {
+
+		// para llamar al metodo de la clase padre
+		// usamos la palabra clave "super".
+		return super.sumar(argumento1, argumento2);
+	}
 
 }
